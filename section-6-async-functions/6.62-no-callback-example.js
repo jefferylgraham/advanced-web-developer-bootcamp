3df7051b02a9eeabb82d0034efa36111afd3b545
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-04-15 18:39:42 -0400

    add code with no callback

---
+++ section-6-async-functions/6.62-no-callback-example.js
@@ -0,0 +1,12 @@
+function sendMessageConsole(message) {
+  console.log(message);
+}
+
+function sendMessageAlert(message) {
+  alert(message);
+}
+function sendMessageConfirm(message) {
+  confirm(message);
+}
+
+sendMessageAlert("Lots of duplication.")
