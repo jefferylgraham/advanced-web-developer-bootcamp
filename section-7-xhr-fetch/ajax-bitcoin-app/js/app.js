895642e7c323ce95e118ca28dc8cfb0f0afd0430
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-04-23 21:55:45 -0400

    add beta of bitcoin app exercise

---
+++ section-7-xhr-fetch/ajax-bitcoin-app/js/app.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector("#btn");
+var price = document.querySelector("#price");
+
+btn.addEventListener("click", function() {
+  var XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      var data = JSON.parse(XHR.responseText);
+      var currentRate = data.bpi.USD.rate;
+      price.append(currentRate + " USD");
+    }
+  }
+
+  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json")
+  XHR.send();
+})
