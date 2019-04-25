dee718763d60f2e5eeb4834ee61d0d66b65ea2ad
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-04-24 23:06:14 -0400

    refactor code

--- section-7-xhr-fetch/random-user-generator/js/app.js
+++ section-7-xhr-fetch/random-user-generator/js/app.js
@@ -1,13 +1,12 @@
+var url = "https://randomuser.me/api/";
 var btn = document.querySelector("#btn");
-var fullName = document.querySelector("#fullname")
+var fullNameDisp = document.querySelector("#fullname")
 var userName = document.querySelector("#username");
 var email = document.querySelector("#email");
 var city = document.querySelector("#city");
 var avatar = document.querySelector("#avatar")
 
 btn.addEventListener("click", function() {
-  var url = "https://randomuser.me/api/";
-
   fetch(url)
   .then(handleErrors)
   .then(parseJSON)
@@ -23,15 +22,19 @@ function handleErrors(request) {
 }
 
 function parseJSON(response) {
-  return response.json();
+  return response.json().then(function(parsedData) {
+    return parsedData.results[0];
+  })
 }
 
 function updateProfile(data) {
-  fullName.innerText = data.results[0].name.first + " " + data.results[0].name.last;
-  username.innerText = data.results[0].login.username;
-  email.innerText = data.results[0].email;
-  city.innerText = data.results[0].location.city;
-  avatar.src = data.results[0].picture.medium;
+  var fullname = data.name.first + " " + data.name.last;
+
+  fullNameDisp.innerText = fullname;
+  username.innerText = data.login.username;
+  email.innerText = data.email;
+  city.innerText = data.location.city;
+  avatar.src = data.picture.medium;
 }
 
 function printError(err) {
