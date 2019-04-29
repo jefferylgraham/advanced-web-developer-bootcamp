c6972c89437c17d63548e004ec008b5e5f04b99b
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-04-28 21:25:28 -0400

    add xhr response

--- section-8-ajax-axios/ron-swanson-quote-generator/js/app.js
+++ section-8-ajax-axios/ron-swanson-quote-generator/js/app.js
@@ -1,1 +1,17 @@
 var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+var quote = document.querySelector("#quote");
+var xhrBtn = document.querySelector("#xhr");
+
+
+xhrBtn.addEventListener("click", function() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var xhrText = JSON.parse(xhr.responseText)[0];
+      quote.innerText = xhrText;
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
+});
