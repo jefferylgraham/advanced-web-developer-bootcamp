93abd52910a9a83cd86920ccdd2d916c25fda036
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-02 22:26:39 -0400

    add beforeAll exercise

---
+++ section-9-jasmine/beforeAll/js/spec.js
@@ -0,0 +1,24 @@
+// beforeAll/afterAll: runs before all DataTransferItemList. DOES NOT RESET IN BETWEEN
+var arr = [];
+beforeAll(function() {
+  arr = [1, 2, 3];
+});
+
+describe("Counting", function() {
+  it("starts with an array", function() {
+    arr.push(4);
+    expect(1).toBe(1);
+  });
+
+  it("keeps mutating that array", function() {
+    arr.push(5);
+    expect(1).toBe(1);
+  });
+});
+
+describe("Again", function() {
+  it("keeps mutating that array...again", function() {
+    console.log(arr);
+    expect(1).toBe(1);
+  })
+})
