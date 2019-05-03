256ba5f746f8cc299a0ea3e7e29a2e6c32df852a
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-05-02 22:42:22 -0400

    add pendong tests exercise

---
+++ section-9-jasmine/pending-tests/js/spec.js
@@ -0,0 +1,12 @@
+describe("Pending specs", function() {
+  xit("can start with xit", function() {
+    expect(true).toBe(true);
+  });
+
+  it("is a pending test if there is no callback");
+
+  it("is pending if the pending function is invoked inside the callback", function() {
+    expect(2).toBe(2);
+    pending();
+  });
+});
